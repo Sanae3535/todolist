7a826bd1f4033cf2c4ee89b7fceeff1db2e910c7
Sanae3535 <elmghari.sanae.23@ump.ac.ma>
2026-02-04 12:23:25 +0100

    new todolist

---
+++ database/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "todoliste";
+
+$db = mysqli_connect($host, $username, $password, $database);
+
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
